f21134d2b5e708f1812840a85b34e0b4ce5e4ea4
prabhjot98 <pssandhu@ucdavis.edu>
2023-03-28 10:31:47 -0700

    make it go fast
    actually work

--- lua/rekt/init.lua
+++ lua/rekt/init.lua
@@ -1,11 +1,11 @@
-local utils = require('rekt.utils')
+local utils = require("rekt.utils")
 
 ---@class RektConfig
 local default_config = {
   ---@class RektSplitConfig
   split_options = {
     ---@type 'vsplit' | 'hsplit'
-    direction = 'vsplit'
+    direction = "vsplit",
   },
 }
 
@@ -23,9 +23,10 @@ end
 function M.open_test_file()
   local filename = vim.api.nvim_buf_get_name(0)
   local basename = vim.fs.basename(filename)
+  local pathname = vim.fs.dirname(filename)
 
   local test_name = utils.make_test_name(basename)
-  local test_name_matches = vim.fs.find(test_name, { limit = math.huge })
+  local test_name_matches = vim.fs.find(test_name, { limit = math.huge, path = pathname })
 
   local edit_file = nil
   if #test_name_matches == 0 then
