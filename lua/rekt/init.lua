d14ab8f781b4130922c3732cad04712a3f4fd674
gigamaax <57466120+gigamaax@users.noreply.github.com>
2023-06-18 10:37:26 -0700

    chore: formatting

--- lua/rekt/init.lua
+++ lua/rekt/init.lua
@@ -1,12 +1,12 @@
-local utils = require('rekt.utils')
+local utils = require("rekt.utils")
 
 ---@class RektConfig
 local default_config = {
-  ---@class RektSplitConfig
-  split_options = {
-    ---@type 'vsplit' | 'hsplit'
-    direction = 'vsplit'
-  },
+	---@class RektSplitConfig
+	split_options = {
+		---@type 'vsplit' | 'hsplit'
+		direction = "vsplit",
+	},
 }
 
 local M = {}
@@ -15,31 +15,31 @@ M.config = default_config
 
 ---@param opt RektConfig | nil
 function M.setup(opt)
-  if opt ~= nil then
-    M.config = opt
-  end
+	if opt ~= nil then
+		M.config = opt
+	end
 end
 
 ---@param from_path string The path to start searching deafults to same directory
 function M.open_test_file(from_path)
-  local filename = vim.api.nvim_buf_get_name(0)
-  local basename = vim.fs.basename(filename)
+	local filename = vim.api.nvim_buf_get_name(0)
+	local basename = vim.fs.basename(filename)
 
-  from_path = from_path or vim.fs.dirname(filename)
-  local edit_file = string.format("%s/%s", from_path, utils.make_test_name(basename))
+	from_path = from_path or vim.fs.dirname(filename)
+	local edit_file = string.format("%s/%s", from_path, utils.make_test_name(basename))
 
-  utils.edit_file(M.config.split_options, edit_file)
+	utils.edit_file(M.config.split_options, edit_file)
 end
 
 ---@param from_path string The path to start searching deafults to same directory
 function M.open_source_file(from_path)
-  local filename = vim.api.nvim_buf_get_name(0)
-  local basename = vim.fs.basename(filename)
+	local filename = vim.api.nvim_buf_get_name(0)
+	local basename = vim.fs.basename(filename)
 
-  from_path = from_path or vim.fs.dirname(filename)
-  local edit_file = string.format("%s/%s", from_path, utils.make_source_name(basename))
+	from_path = from_path or vim.fs.dirname(filename)
+	local edit_file = string.format("%s/%s", from_path, utils.make_source_name(basename))
 
-  utils.edit_file(M.config.split_options, edit_file)
+	utils.edit_file(M.config.split_options, edit_file)
 end
 
 return M
--- lua/rekt/utils.lua
+++ lua/rekt/utils.lua
@@ -1,14 +1,14 @@
 local extratypes = {
-  ts = "typescript",
-  tsx = "typescript",
+	ts = "typescript",
+	tsx = "typescript",
 }
 
 local test_ext = {
-  go = "_test",
-  javascript = ".spec",
-  lua = ".test",
-  typescript = ".spec",
-  typescriptreact = ".spec", -- this is weird
+	go = "_test",
+	javascript = ".spec",
+	lua = ".test",
+	typescript = ".spec",
+	typescriptreact = ".spec", -- this is weird
 }
 
 local M = {}
@@ -16,42 +16,42 @@ local M = {}
 ---@param filename string
 ---@return string
 function M.guess_type(filename)
-  local type = vim.filetype.match({ filename = filename, })
-  if type ~= "" and type ~= nil then
-    return type
-  end
+	local type = vim.filetype.match({ filename = filename, })
+	if type ~= "" and type ~= nil then
+		return type
+	end
 
-  local tokens = vim.split(filename, ".", { plain = true, trimempty = true, })
-  return extratypes[tokens[#tokens]]
+	local tokens = vim.split(filename, ".", { plain = true, trimempty = true, })
+	return extratypes[tokens[#tokens]]
 end
 
 ---@param filename string
 ---@return string
 function M.make_test_name(filename)
-  local filetype = M.guess_type(filename)
-  return (string.gsub(filename, "(.*)(%.)(%w+)", "%1" .. test_ext[filetype] .. ".%3"))
+	local filetype = M.guess_type(filename)
+	return (string.gsub(filename, "(.*)(%.)(%w+)", "%1" .. test_ext[filetype] .. ".%3"))
 end
 
 ---@param filename string
 ---@return string
 function M.make_source_name(filename)
-  local filetype = M.guess_type(filename)
-  return (string.gsub(filename, "(.*)" .. test_ext[filetype] .. ".(%w+)", "%1.%2"))
+	local filetype = M.guess_type(filename)
+	return (string.gsub(filename, "(.*)" .. test_ext[filetype] .. ".(%w+)", "%1.%2"))
 end
 
 ---@param config RektSplitConfig | nil
 ---@param file string
 function M.edit_file(config, file)
-  if config ~= nil then
-    if config.direction == "vsplit" then
-      vim.cmd.vsplit()
-    else
-      vim.cmd.split()
-    end
-  end
+	if config ~= nil then
+		if config.direction == "vsplit" then
+			vim.cmd.vsplit()
+		else
+			vim.cmd.split()
+		end
+	end
 
-  -- TOOD: will need to handle multiple file names or when the file doesn't exist
-  vim.cmd.e(file)
+	-- TOOD: will need to handle multiple file names or when the file doesn't exist
+	vim.cmd.e(file)
 end
 
 return M
