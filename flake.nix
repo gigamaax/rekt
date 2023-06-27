6f35d4af3cd1faac48e9f5e8783e8646411bbf1c
gigamaax <57466120+gigamaax@users.noreply.github.com>
2023-06-27 11:16:38 -0700

    fix: lsp

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
-  description = "rekt development environment";
+	description = "rekt development environment";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+	inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        {
-          devShell = pkgs.mkShell {
-            name = "rekt";
-            buildInputs = with pkgs; [
-              just
-              watchexec
-            ];
-          };
-        }
-      );
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem
+			(system: let
+				pkgs = nixpkgs.legacyPackages.${system};
+			in
+				{
+					devShell = pkgs.mkShell {
+						name = "rekt";
+						buildInputs = with pkgs; [
+							just
+							watchexec
+						];
+					};
+				}
+			);
 }
